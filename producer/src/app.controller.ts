d96c1f44c0c6f87c7ea596fdfc63c6fc85fc42b5
unknown <ermessonlimadossantos@hotmail.com>
2022-06-15 02:29:24 -0300

    feat:microservices

---
+++ producer/src/app.controller.ts
@@ -0,0 +1,25 @@
+import { Controller, Get } from '@nestjs/common';
+import { Client, ClientKafka, ClientRMQ, Transport } from '@nestjs/microservices';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Client( {
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://localhost:5672'],
+      queue: 'storage-ec',
+      queueOptions: { durable: false },
+    }
+  })
+  client: ClientRMQ;
+
+  @Get()
+  sendToStorage() {
+    return this.client.send('storage-ec',{
+      message: 'Remove pepe from storage'
+    });
+  }
+}
